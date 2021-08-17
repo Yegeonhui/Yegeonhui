5356574b9eb55dbfd9e9ce4f1bd8a1c06c57f6fb
Yegeonhui <dprjsgml@gmail.com>
2021-08-17 11:35:11 +0900

    no message

--- Geonhui/extra/객체지향/example/Programmer.py
+++
@@ -1,9 +0,0 @@
-#클래스와 인스턴스
-class Programmer:
-    def web(self): #메서드 
-        return "web_pro"
-    def app(self):
-        return "app_pro"
-    def ai(self):
-        return "ai_pro"
-
--- Geonhui/test.py
+++
@@ -1,3 +0,0 @@
-import os
-route=os.getcwd()
-print(route)
