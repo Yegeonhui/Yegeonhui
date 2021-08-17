70af2c650cb082c48216cb7607096c96cbf703d5
Guniye <dprjsgml@gmail.com>
2021-08-17 10:18:11 +0900

    Revert "no message"
    This reverts commit 6e6aa6a4e4a030570cddba2dc01975ec77a3af8c.

--- Geonhui/Assignment03/1.Convolution/comparison.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-cafe_for=cv2.imread("cafe_Convolution5.jpg")
-cafe_opencv=cv2.imread("cafe_opencv.jpg")
-merged=np.hstack((cafe_for,cafe_opencv))
-cv2.imshow("comparsion",merged)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
